9797d6ed3782778371cce272ad809577943f4889
robin <robinnagpal.tiet@gmail.com>
2022-01-30 11:44:27 -0500

    Add mission and the update the space request

--- src/helpers/onboardingClient.ts
+++ src/helpers/onboardingClient.ts
@@ -1,5 +1,7 @@
 import { guideTypes } from '@/helpers/sign/guideTypes';
+import { spaceTypes } from '@/helpers/sign/spaceTypes';
 import { GuideInput } from '@dodao/onboarding-schemas/inputs/GuideInput';
+import { SpaceInput } from '@dodao/onboarding-schemas/inputs/SpaceInput';
 import { Web3Provider } from '@ethersproject/providers';
 import { Wallet } from '@ethersproject/wallet';
 import Client from '@snapshot-labs/snapshot.js/src/sign';
@@ -11,5 +13,14 @@ export default class OnboardingClient extends Client {
     message: Omit<GuideInput, 'timestamp' | 'from'>
   ) {
     return await this.sign(web3, address, message, guideTypes);
+  }
+
+  async spaceNew(
+    web3: Web3Provider | Wallet,
+    address: string,
+    message: SpaceInput
+  ) {
+    console.log('write space', message);
+    return await this.sign(web3, address, message, spaceTypes);
   }
 }
---
+++ src/helpers/sign/spaceTypes.ts
@@ -0,0 +1,21 @@
+export const spaceTypes = {
+  Settings: [
+    { name: 'about', type: 'string' },
+    { name: 'admins', type: 'address[]' },
+    { name: 'avatar', type: 'string' },
+    { name: 'creator', type: 'address' },
+    { name: 'categories', type: 'string[]' },
+    { name: 'github', type: 'string' },
+    { name: 'members', type: 'address[]' },
+    { name: 'mission', type: 'string' },
+    { name: 'network', type: 'string' },
+    { name: 'terms', type: 'string' },
+    { name: 'twitter', type: 'string' }
+  ],
+  Space: [
+    { name: 'from', type: 'address' },
+    { name: 'space', type: 'string' },
+    { name: 'timestamp', type: 'uint64' },
+    { name: 'settings', type: 'Settings' }
+  ]
+};
