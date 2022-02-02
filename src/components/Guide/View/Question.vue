61dbaf4af6d33594b98ad531618560855af99c50
robin <robinnagpal.tiet@gmail.com>
2022-02-01 22:25:26 -0500

    Fix checkbox alignment

--- src/components/Guide/View/Question.vue
+++ src/components/Guide/View/Question.vue
@@ -15,12 +15,13 @@ defineProps({
   <div class="border md:rounded-lg p-4 mb-4 bg-skin-block-bg">
     <UiMarkdown :body="question.content" class="mb-6" />
     <template v-for="choice in question.choices" :key="choice.key">
-      <div class="flex">
+      <div class="flex leading-loose">
         <Checkbox
           @update:modelValue="selectAnswer(question.uuid, choice.key, $event)"
           :modelValue="question.answerKeys.includes(choice.key)"
         />
-        {{ choice.content }}
+        <div class="mt-2">{{ choice.content }} </div>
+
       </div>
     </template>
   </div>
