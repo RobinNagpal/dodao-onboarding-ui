764e4c86f7af9f3c09dd8d777d6c335948d5ca0d
jpuri <jyotipuri@gmail.com>
2022-02-15 15:03:23 +0530

    Adding back button on DAO page

--- src/components/Layout/TopAndBottom.vue
+++ src/components/Layout/TopAndBottom.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+import Icon from '@/components/Icon.vue';
+import { computed } from 'vue';
+
+const browserHasHistory = computed(() => window.history.state.back);
+</script>
+
 <template>
   <div class="section mt-4">
     <div class="container-default">
@@ -7,6 +14,19 @@
           <div
             class="top-content card integration-main-card integration-card-top-content"
           >
+            <div class="px-4 md:px-0">
+              <a
+                class="text-color"
+                @click="
+                  browserHasHistory
+                    ? $router.go(-1)
+                    : $router.push(domain ? { path: '/' } : { name: 'guide' })
+                "
+              >
+                <Icon name="back" size="22" class="!align-middle" />
+                {{ browserHasHistory ? $t('back') : space.name }}
+              </a>
+            </div>
             <div
               class="flex integration-top-content"
               v-if="$slots['top-content']"
