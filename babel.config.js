68c8200f334e2cdb3fd9c4fae73636e67c43ea1b
robin <robinnagpal.tiet@gmail.com>
2022-03-14 13:54:28 -0400

    use local copy of node modules

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,26 @@
+function excludeCondition(path) {
+  const nonEs5SyntaxPackages = ['@solana'];
+
+  // DO transpile these packages
+  if (nonEs5SyntaxPackages.some(pkg => path.toString().indexOf(pkg)) > -1) {
+    console.log('\n Will transpile - ' + path);
+    return false;
+  }
+
+  // Ignore all other modules that are in node_modules
+  if (path.toString().replace(__dirname, '').startsWith('/node_modules')) {
+    console.log('\n Will not transpile - ' + path);
+    return true;
+  } else {
+    console.log('\n Will transpile - ' + path);
+    return false;
+  }
+}
+
 module.exports = {
-  presets: ['@vue/cli-plugin-babel/preset']
+  exclude: excludeCondition,
+  presets: [
+    ['@babel/preset-env', { forceAllTransforms: true }],
+    ['@vue/cli-plugin-babel/preset', { useBuiltIns: false }]
+  ]
 };
