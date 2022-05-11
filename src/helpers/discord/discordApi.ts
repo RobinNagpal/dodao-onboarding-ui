a09b5331be7e5c631ca7d0dda8a3d755a2da4840
robin <robinnagpal.tiet@gmail.com>
2022-05-11 17:32:18 -0400

    remove unused dependency

--- src/helpers/discord/discordApi.ts
+++ src/helpers/discord/discordApi.ts
@@ -1,5 +1,3 @@
-import { APIGuild } from 'discord-api-types/payloads/v10/guild';
-
 function getDiscordApiResponse(apiUrl: string, accessToken: string) {
   return fetch(apiUrl, {
     headers: {
@@ -11,12 +9,4 @@ function getDiscordApiResponse(apiUrl: string, accessToken: string) {
 export async function getGuilds(accessToken: string): Promise<any[]> {
   const apiUrl = 'https://discord.com/api/users/@me/guilds';
   return (await getDiscordApiResponse(apiUrl, accessToken)) as any[];
-}
-
-export async function getGuild(
-  accessToken: string,
-  guildId: string
-): Promise<APIGuild> {
-  const apiUrl = `https://discord.com/api/guilds/${guildId}`;
-  return (await getDiscordApiResponse(apiUrl, accessToken)) as APIGuild;
 }
