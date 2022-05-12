e7daf08ddd8ec662ba131e60272e879ccc80f613
robin <robinnagpal.tiet@gmail.com>
2022-05-12 00:42:55 -0400

    Integrate setting of the discord server

--- src/helpers/discord/discordApi.ts
+++ src/helpers/discord/discordApi.ts
@@ -1,3 +1,10 @@
+import {
+  SpaceDiscordGuild as SpaceDiscordGuildResult,
+  SpaceDiscordGuildVariables
+} from '@/graphql/generated/graphqlDocs';
+import { SpaceDiscordGuild } from '@/graphql/space/extendedSpace.graphql';
+import { useApolloClient } from '@vue/apollo-composable';
+
 function getDiscordApiResponse(apiUrl: string, accessToken: string) {
   return fetch(apiUrl, {
     headers: {
@@ -9,4 +16,17 @@ function getDiscordApiResponse(apiUrl: string, accessToken: string) {
 export async function getGuilds(accessToken: string): Promise<any[]> {
   const apiUrl = 'https://discord.com/api/users/@me/guilds';
   return (await getDiscordApiResponse(apiUrl, accessToken)) as any[];
+}
+
+export async function getSelectedGuild(spaceId: string): Promise<any> {
+  const client = useApolloClient();
+  const { data } = await client.client.query<
+    SpaceDiscordGuildResult,
+    SpaceDiscordGuildVariables
+  >({
+    query: SpaceDiscordGuild,
+    variables: { spaceId }
+  });
+
+  return data.payload;
 }
--- src/views/Space/Discord.vue
+++ src/views/Space/Discord.vue
@@ -1,25 +1,17 @@
 <script setup lang="ts">
 import LayoutSingle from '@/components/Layout/Single.vue';
 import UiButton from '@/components/Ui/Button.vue';
-import UiDropdown from '@/components/Ui/Dropdown.vue';
 import { useExtendedSpaces } from '@/composables/useExtendedSpaces';
 import {
   AddDiscordCredentials_payload,
-  AddDiscordCredentialsVariables,
-  SetDiscordGuildId_payload,
-  SetDiscordGuildIdVariables,
-  SpaceDiscordGuild as SpaceDiscordGuildResult
+  AddDiscordCredentialsVariables
 } from '@/graphql/generated/graphqlDocs';
-import { SpaceDiscordGuild } from '@/graphql/space/extendedSpace.graphql';
-import {
-  AddDiscordCredentials,
-  SetDiscordGuildId
-} from '@/graphql/space/spaceDiscord.mutation.graphql';
-import { getGuilds } from '@/helpers/discord/discordApi';
-import { genPermissions } from '@/helpers/discord/discordPermissions';
+
+import { AddDiscordCredentials } from '@/graphql/space/spaceDiscord.mutation.graphql';
+import { getSelectedGuild } from '@/helpers/discord/discordApi';
 import { setPageTitle } from '@/helpers/utils';
 import { SpaceModel } from '@dodao/onboarding-schemas/models/SpaceModel';
-import { useMutation, useQuery } from '@vue/apollo-composable';
+import { useMutation } from '@vue/apollo-composable';
 import { computed, onMounted, PropType, ref } from 'vue';
 import { useRoute } from 'vue-router';
 
@@ -55,22 +47,7 @@ const { mutate: addDiscordCredentialsMutation } = useMutation<
   AddDiscordCredentialsVariables
 >(AddDiscordCredentials);
 
-const { mutate: setDiscordGuildIdMutation } = useMutation<
-  SetDiscordGuildId_payload,
-  SetDiscordGuildIdVariables
->(SetDiscordGuildId);
-
-const discordServerOpts = ref<any[]>([]);
 const selectedServerInfo = ref<any>(null);
-
-async function handleSelectServer(item) {
-  selectedServerInfo.value = item;
-  await setDiscordGuildIdMutation({
-    guildId: item.id,
-    spaceId: props.space.id
-  });
-  await loadExtendedSpace(props.space.id);
-}
 
 onMounted(async () => {
   setPageTitle('page.title.home');
@@ -80,29 +57,9 @@ onMounted(async () => {
       redirectUri: `${window.location.origin}/generic-discord-redirect`,
       spaceId: props.space.id
     });
-    await loadExtendedSpace(props.space.id);
   }
-
-  const accessToken = props.space.discordAccessToken;
-  if (accessToken) {
-    const allGuilds = await getGuilds(accessToken);
-
-    discordServerOpts.value = allGuilds.filter(guild => {
-      return genPermissions(guild.permissions_new).includes('MANAGE_GUILD');
-    });
-    if (props.space.discordGuildId) {
-      const { onResult } = await useQuery<SpaceDiscordGuildResult>(
-        SpaceDiscordGuild,
-        {
-          spaceId: props.space.id
-        }
-      );
-      onResult(result => {
-        console.log('queryReturn', result.data.payload);
-        selectedServerInfo.value = result.data.payload;
-      });
-    }
-  }
+  await loadExtendedSpace(props.space.id);
+  selectedServerInfo.value = await getSelectedGuild(props.space.id);
 });
 </script>
 
@@ -110,45 +67,23 @@ onMounted(async () => {
   <LayoutSingle v-bind="$attrs">
     <template #content>
       <h1 v-text="$t('setupDAO.header')" class="flex-1" />
-      <div class="py-24 flex justify-center align-center">
-        <div class="px-4 md:px-0 mb-4">
+      <div class="py-24 flex justify-center align-center flex-col items-center">
+        <div class="my-6">
           <div v-if="space.discordAccessToken">
             {{ 'Discord Access Token: ' + space.discordAccessToken }}
           </div>
           <UiButton>
             <a :href="url">Connect Discord</a>
           </UiButton>
-          <div>
-            <UiDropdown
-              top="2.5rem"
-              right="1.5rem"
-              class="inline-block mt-4"
-              :items="discordServerOpts"
-              @select="handleSelectServer($event)"
-            >
-              <UiButton class="discord-server-btn" v-if="selectedServerInfo">
-                <img
-                  class="h-[24px] mr-2"
-                  :src="`https://cdn.discordapp.com/icons/${selectedServerInfo.id}/${selectedServerInfo.icon}.png`"
-                />
-                <span>{{ selectedServerInfo.name }}</span>
-              </UiButton>
-              <UiButton class="discord-server-btn" v-else>
-                <span>Choose Discord Server</span>
-              </UiButton>
-              <template v-slot:item="{ item }">
-                <div class="">
-                  <div class="flex mr-4 items-center">
-                    <img
-                      class="h-[24px] mr-2"
-                      :src="`https://cdn.discordapp.com/icons/${item.id}/${item.icon}.png`"
-                    />
-                    <span>{{ item.name }}</span>
-                  </div>
-                </div>
-              </template>
-            </UiDropdown>
-          </div>
+        </div>
+        <div class="my-6">
+          <UiButton class="discord-server-btn" v-if="selectedServerInfo">
+            <img
+              class="h-[24px] mr-2"
+              :src="`https://cdn.discordapp.com/icons/${selectedServerInfo.id}/${selectedServerInfo.icon}.png`"
+            />
+            <span>{{ selectedServerInfo.name }}</span>
+          </UiButton>
         </div>
       </div>
     </template>
